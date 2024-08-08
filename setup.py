936103f2207d2b98800eaf28c8bbf638e74f266f
cedric <cedricpuystjens@outlook.com>
2024-08-08 14:37:33 +0200

    add icon and setup.py for cxfreeze

---
+++ setup.py
@@ -0,0 +1,20 @@
+'''setup for cx_freeze'''
+
+from cx_Freeze import setup, Executable
+
+# Define the executable
+executables = [
+    Executable(
+        script="gui_password_generator.py",
+        base="Win32GUI",
+        icon="assets/lock_and_key.ico"
+    )
+]
+
+# Setup configuration
+setup(
+    name="Password Generator",
+    version="1.0",
+    description="Password generator",
+    executables=executables
+)
